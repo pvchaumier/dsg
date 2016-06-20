b5d163fadec93503fa88b4ee7e2da415a9baa56e
Pierre-Victor Chaumier <chaumierpv@gmail.com>
2016-06-20 22:27:58 +0200

    Add first functions.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,19 +1,28 @@
 """Preprocess image."""
 
+
 def rotation(img, angle):
     pass
 
+
 def transpose(img):
-    pass
+    """Transpose the image."""
+    return img[::-1,::-1,:]
+
 
 def symmetry_horiz(img):
-    pass
+    """Flips the image along an horizontal axis."""
+    return img[::-1,:,:]
+
 
 def symmetry_vertical(img):
-    pass
+    """Flips the image along an vertical axis."""
+    return img[:,::-1,:]
+
 
 def resize(img, new_size=80):
     pass
+
 
 def kmeans(img, k=20):
     pass
