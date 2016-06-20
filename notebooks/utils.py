011a28ce4ed0adf2bfc7d2e1ff7d5528bea0130c
Pierre-Victor Chaumier <chaumierpv@gmail.com>
2016-06-20 22:00:26 +0200

    Add utils.py file with function to complete

---
+++ notebooks/utils.py
@@ -0,0 +1,19 @@
+"""Preprocess image."""
+
+def rotation(img, angle):
+    pass
+
+def transpose(img):
+    pass
+
+def symetry_horiz(img):
+    pass
+
+def symetry_vertical(img):
+    pass
+
+def resize(img, new_size=80):
+    pass
+
+def kmeans(img, k=20):
+    pass
